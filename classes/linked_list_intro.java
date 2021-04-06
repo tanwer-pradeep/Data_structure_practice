be17e56038bd55f1ca8a4142fd9ad17e0854f80a
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-04-06 17:24:54 +0530

    my first commit

---
+++ classes/linked_list_intro.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class linked_list_intro {
+    // makinng node class
+    public static class Node{
+        int data;
+        Node next;
+    }    
+
+    public static class LinnkedList{
+        Node head, tail;
+        int size;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
