7a9f5353729418257caa534aaa94e9ce8e5653c7
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-05-11 21:00:02 +0530

    perfect friend solution

--- graph/Perfect_friend.java
+++ graph/Perfect_friend.java
@@ -15,26 +15,38 @@ public class Main {
       }
    }
    
-   public static void main(String[] args) throws Exception {
-      Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
 
-      int n = scn.nextInt();
-      int k = scn.nextInt();
-      
-      ArrayList<Edge>[] graph = new ArrayList[n];
-      for(int i = 0; i < n; i++){
-         graph[i] = new ArrayList<>();
-      }
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            graph[i] = new ArrayList<>();
+        }
 
-      for(int i = 0; i < k; i++){
-         int v1 = scn.nextInt();
-         int v2 = scn.nextInt();
-         int wt = 0;
-         graph[v1].add(new Edge(v1, v2, wt));
-         graph[v2].add(new Edge(v2, v1, wt));
+        for(int i = 0; i < k; i++){
+            int v1 = scn.nextInt();
+            int v2 = scn.nextInt();
+            int wt = 0;
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+      
+      ArrayList<ArrayList<Integer>> allcomp = gcc(graph);
+      int res = 0;
+      for(int i = 0; i < allcomp.size(); i++){
+        for(int j = 0; j < allcomp.size(); j++){
+            ArrayList<integer> ith = allcomp.get(i);
+            ArrayList<integer> jth = allcomp.get(j);
+
+            res += (ith.size() * jth.size());
+        }
       }
-      
-      
-   }
+      System.out.println(res);
+    }
+
+
 
 }
